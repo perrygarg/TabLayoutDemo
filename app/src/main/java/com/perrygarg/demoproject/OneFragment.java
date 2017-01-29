dcc6a416e077c7310b251cd9b43c8064c95b7b16
Perry Garg <Perry Garg>
2017-01-29 23:34:23 +0530

    Initial Commit

---
+++ app/src/main/java/com/perrygarg/demoproject/OneFragment.java
@@ -0,0 +1,30 @@
+package com.perrygarg.demoproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by PerryGarg on 29-01-2017.
+ */
+public class OneFragment extends Fragment {
+
+    public OneFragment()
+    {
+        //required empty constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
---
+++ app/src/main/java/com/perrygarg/demoproject/ThreeFragment.java
@@ -0,0 +1,30 @@
+package com.perrygarg.demoproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by PerryGarg on 29-01-2017.
+ */
+public class ThreeFragment extends Fragment {
+
+    public ThreeFragment()
+    {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return  inflater.inflate(R.layout.fragment_three, container, false);
+    }
+}
