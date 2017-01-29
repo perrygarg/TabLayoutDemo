fa8645d4d5103fe0db1bcd40ae1debcdc0b68cee
PerryGarg <perrygarg.1993@gmail.com>
2017-01-30 00:32:28 +0530

    Main Activity commit test 2

--- app/src/main/java/com/perrygarg/demoproject/MainActivity.java
+++ app/src/main/java/com/perrygarg/demoproject/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //perry garg jdhehdjedh
+    //perry garg jdhehdjedh testing
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
