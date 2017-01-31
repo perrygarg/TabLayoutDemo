032790fafa659ea11ffdee07bb59c98687cf4c7a
akash.singla <akash.singla@naukri.com>
2017-01-31 11:25:32 +0530

    Add Tabs................

--- app/src/main/java/com/perrygarg/demoproject/MainActivity.java
+++ app/src/main/java/com/perrygarg/demoproject/MainActivity.java
@@ -35,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+
+        TabLayout.Tab tab1 = tabLayout.newTab();
+        tab1.setText("kaka1");
+        tabLayout.addTab(tab1);
+
+        TabLayout.Tab tab2 = tabLayout.newTab();
+        tab2.setText("kaka2");
+        tabLayout.addTab(tab2);
+
+        TabLayout.Tab tab3 = tabLayout.newTab();
+        tab3.setText("kaka3");
+        tabLayout.addTab(tab3);
+
     }
 
     private void setupViewPager(ViewPager viewPager)
